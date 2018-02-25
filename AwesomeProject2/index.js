fbd0cc34f02fb54727701dc6a535a30f1ac71c07
danielalza <36808000+danielalza@users.noreply.github.com>
2018-02-25 00:52:52 -0500

    entraga
    prueba de react-native

---
+++ AwesomeProject2/index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+//import App from './App';
+import loginView from './src/componentes/loginView';
+
+AppRegistry.registerComponent('AwesomeProject2', () => loginView);
